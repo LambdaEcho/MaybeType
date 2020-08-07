d159760e7c19413988fad06ca22579e6b684349e
Lambda Echo <d>
2020-08-07 15:12:34 +0200

    Implementation of ISerializable added

---
+++ src/MaybeType.Tests/MaybeTests.ISerializable.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using FluentAssertions;
+using LambdaEcho.MaybeType;
+using Xunit;
+
+namespace MaybeType.Tests
+{
+    public partial class MaybeTests
+    {
+        [Theory]
+        [MemberData(nameof(ISerialize___TestData))]
+        public void ISerialize___Serialization_and_Deserialization___Work_Properly(object maybe, string expectedSerializedString)
+        {
+            // Arrange
+            string serializedString;
+            var binaryFormatter = new BinaryFormatter();
+
+            using (var memoryStream = new MemoryStream())
+            {
+                // Act Serialization
+                binaryFormatter.Serialize(memoryStream, maybe);
+
+                // Assert Serialization
+                memoryStream.Position = 0;
+                using (var streamReader = new StreamReader(memoryStream))
+                {
+                    serializedString = streamReader.ReadToEnd();
+                }
+            }
+
+            serializedString.Should().EndWithEquivalent(expectedSerializedString);
+
+            // Act DeSerialization
+            var byteArray = Encoding.ASCII.GetBytes(serializedString);
+            using (var memoryStream = new MemoryStream(byteArray))
+            {
+                var reconstructedMaybe = binaryFormatter.Deserialize(memoryStream);
+
+                // Assert DeSerialization
+                maybe.Equals(reconstructedMaybe).Should().BeTrue();
+            }
+        }
+
+        public static IEnumerable<object[]> ISerialize___TestData()
+        {
+            return new List<object[]>
+            {
+                new object[]
+                {
+                    Maybe.Create("Hello world!"), "\u0001h\u0001m\0\u0001\u0001\u0002\0\0\0\u0001\u0006\u0003\0\0\0\fHello world!\v"
+                },
+                new object[] { Maybe.Nothing<string>(), "\u0001h\u0001m\0\u0001\u0001\u0002\0\0\0\0\n\v" },
+                new object[] { Maybe.Create(42), "\u0001h\u0001m\0\0\u0001\b\u0002\0\0\0\u0001*\0\0\0\v" },
+                new object[] { Maybe.Nothing<int>(), "\u0001h\u0001m\0\0\u0001\b\u0002\0\0\0\0\0\0\0\0\v" }
+            };
+        }
+    }
+}
---
+++ src/MaybeType/Maybe.ISerializable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace LambdaEcho.MaybeType
+{
+    [Serializable]
+    public readonly partial struct Maybe<T> : ISerializable
+    {
+        /// <summary>
+        ///     Instantiates a new <see cref="Maybe{T}" /> from an serialized stream.
+        /// </summary>
+        public Maybe(SerializationInfo info, StreamingContext context)
+        {
+            HasValue = info.GetBoolean("h");
+            _value = (T) info.GetValue("m", typeof(T));
+        }
+
+        /// <summary>
+        ///     Serializes this <see cref="Maybe{T}" /> to a stream.
+        /// </summary>
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("h", HasValue, typeof(bool));
+            info.AddValue("m", _value, typeof(T));
+        }
+    }
+}
